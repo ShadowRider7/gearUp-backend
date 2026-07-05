a722202b5eb54f5997090d352885e081eb7ea9e8
MD. SAFAYET HOSSAIN SADHIN <sadhinv@gmail.com>
2026-07-06 00:28:55 +0600

    global error handler and not found route added

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import cors from "cors";
 import express, { Application, NextFunction, Request, Response } from "express";
 import config from "./config";
 import cookieParser from "cookie-parser";
+import { notFound } from "./middlewares/notFound";
+import { globalErrorHandler } from "./middlewares/globalErrorHandler";
 const app: Application = express();
 
 app.use(
@@ -16,4 +18,8 @@ app.use(cookieParser());
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello! , world!!");
 });
+
+app.use(notFound);
+
+app.use(globalErrorHandler);
 export default app;
